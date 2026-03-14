a4e7b7164ac100c480de2c9575c6bfbd51fda139
Sudharsan <kamisettysba2027@email.iimcal.ac.in>
2026-03-14 06:49:42 +0530

    web app is completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='Medical-chatbot',
+    version='0.1.0',
+    author="Sudharsan"
+    author_email="kamisettysba2027@email.iimcal.ac.in",
+    packages=find_packages(),
+    install_requires=[]
+)
